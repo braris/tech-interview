d2acaa15111684d51f20825936074569fffbfa43
Pavlishyn <dpavlishyn@luxoft.com>
2024-09-09 09:38:07 +0300

    small change

--- src/main/java/com/github/braris/techinterview/controllers/UserController.java
+++ src/main/java/com/github/braris/techinterview/controllers/UserController.java
@@ -3,10 +3,9 @@ package com.github.braris.techinterview.controllers;
 import com.github.braris.techinterview.dto.user.AccountDto;
 import com.github.braris.techinterview.dto.user.UserDetailDto;
 import com.github.braris.techinterview.dto.user.UserDto;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -23,14 +22,23 @@ public class UserController {
                 new UserDto(2, "Mak", "Mary")
         );
     }
-    @GetMapping(path = "/{id}", produces = "application/json")
-    public UserDetailDto getUserById(@PathVariable long id) {
+
+    @PostMapping(path = "/details", consumes = "application/json", produces = "application/json")
+    public List<UserDetailDto> getUserDetailsByIds(@RequestBody @Valid @NotEmpty List<Long> ids) {
         // TODO: implement
-        return new UserDetailDto(
-                new UserDto(1, "Doe", "John"),
-                List.of(
-                        new AccountDto("12344556", new BigDecimal("123.45"), "test document"),
-                        new AccountDto("12344576", new BigDecimal("56778.45"), "test document 2")
+        return List.of(new UserDetailDto(
+                        new UserDto(1, "Doe", "John"),
+                        List.of(
+                                new AccountDto("12344556", new BigDecimal("123.45"), "test document"),
+                                new AccountDto("12344576", new BigDecimal("56778.45"), "test document 2")
+                        )
+                ),
+                new UserDetailDto(
+                        new UserDto(1, "Smith", "Agent"),
+                        List.of(
+                                new AccountDto("654321", new BigDecimal("233.45"), "test document 3"),
+                                new AccountDto("987654", new BigDecimal("76778.45"), "test document 4")
+                        )
                 )
         );
     }
